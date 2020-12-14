841c4131803fbe24de42374a0344144b297e6901
Ludwig Schubert <ludwigschubi1@gmail.com>
2020-12-14 13:59:35 +0100

    Intermediate commit

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-export const helloWorld = () => "HELLO WORLD";
+import {
+  useQuery as useReactQuery,
+  useMutation as useReactMutation,
+} from "react-query";
+import { GraphQLSchema } from "webql-js";
+
+export class WebQLClient {
+  schema: GraphQLSchema;
+
+  constructor(schema: GraphQLSchema) {
+    this.schema = schema;
+  }
+}
+
+export const useQuery = ({ schema, doc }) => "HELLO WORLD";
+
+export const useMutation = () => "HELLO WORLD";
